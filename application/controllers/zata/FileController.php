99ff4d8a3a2c66946c698dfa98e8eea967ee96ce
Ralny Andrade <ralnyandrade@gmail.com>
2019-10-31 11:07:44 -0300

    Criação do Controller para download de templates de importação

---
+++ application/controllers/zata/FileController.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class FileController extends CI_Controller
+{
+    /**
+     * Construtor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('download');
+    }
+
+
+    /**
+     * Como utilizar
+     * 
+     * @param $fileName é o arquivo que vai ser baixado 
+     * 
+     * exemplo:
+     *  <a href="<?=base_url ()?>download_tpl_importacao/tpl_imp_Eventos_utilizacao_salas.csv"> Download do template de importação</a>
+     */
+
+    public function download_tpl_importacao($fileName = NULL)
+    {
+        if ($fileName) 
+        {
+            //path
+            $file = 'files/downloads/tpl_importacao/'.$fileName;
+            
+            // Verifica se o arquivo existe    
+            if (file_exists($file))
+            {
+                // get file content
+                $data = file_get_contents($file);
+                //force download
+                force_download($fileName, $data);
+            } 
+        }
+    }
+}
