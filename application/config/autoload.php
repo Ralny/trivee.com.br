6abae8f1fa6c12a9d9a6865560cba8025075db34
Ralny Andrade <ralnyandrade@gmail.com>
2019-11-04 19:23:05 -0300

    Adicionando nova libraries de Exportação PDF

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session', 'database', 'parser', 'form_validation', 'Log_zata','PHPReport','m_pdf');
+$autoload['libraries'] = array('session', 'database', 'parser', 'form_validation', 'Log_zata','PHPReport','pdfgenerator');
 
 /*
 | -------------------------------------------------------------------
