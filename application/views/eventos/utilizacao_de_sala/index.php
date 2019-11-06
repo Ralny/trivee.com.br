a24c8036e1ca65bb725baf20fae4bb5cfac41d69
Ralny Andrade <ralnyandrade@gmail.com>
2019-11-06 12:42:13 -0300

    Usabilidade

--- application/views/eventos/utilizacao_de_sala/index.php
+++ application/views/eventos/utilizacao_de_sala/index.php
@@ -1,11 +1,7 @@
 <!-- BEGIN WELLS PORTLET-->
 <div class="portlet light">
-	<div class="portlet-title">
-		<div class="caption">
-			<!--Utilização de Salas-->
-		</div>
-	</div>
-	<div class="portlet-body">
+
+<div class="portlet-body">
 
         <div class="row" style="margin: 0 auto; width: 80%">
         
@@ -17,26 +13,31 @@
 
 			<blockquote>
 				<p class="text-center">
-                    Eventos corporativos podem ser nomeados de diferentes maneiras e modalidades, são exemplos: conferência, simpósio, seminário.
-                     Definir como as salas serão utlizadas vai ajudar a criar indicadores de gestão do seu negócio.
+                   " Eventos corporativos podem ser nomeados de diferentes maneiras.
+                     Definir como as salas serão utlizadas vai ajudar a criar indicadores de gestão do seu negócio."
 				</p>
 			</blockquote>
 
-			<h4 class="text-center" style="font-weight: 600"> Recursos adicionais</h4>
-
-            <p class="text-center"><i class="fa fa-check font-green-sharp"></i>
-                    <a href="<?=base_url ()?>download_tpl_importacao/tpl_imp_Eventos_utilizacao_salas.csv"> Download do template de importação</a> 
-            </p>
-	
-			<br><br>
-
 			<div class="text-center">
-				<a class="btn btn-success" href="<?= base_url()?>produtos/cadastrar" style="width: 200px;"> Adicionar Novo</a>
-				<a class="btn btn-default" data-toggle="modal" href="#basic" style="width: 200px;"> Importar</a>
+				<a class="btn btn-success" href="<?= base_url() . $url ?>/cadastrar " style="width: 260px;"> Criar a primeira utilização de sala</a>
+				<a class="btn btn-default" data-toggle="modal" href="#basic" style="width: 270px;"> Importar minhas utilizações de sala</a>
 			</div>
 
-			<br><br>
+			<br>
 
+			<h4 class="text-center" style="font-weight: 600"> Assistente de importação de dados</h4>
+
+			<p class="text-center">
+			         Selecione o arquivo .CSV de até 200 linhas e importe para o ZATA.
+			</p>
+			
+			<p class="text-center">
+				Se preferir,<a href="<?=base_url ()?>download_tpl_importacao/tpl_imp_Eventos_utilizacao_salas.csv">
+						 faça o download da nossa planilha modelo clicando aqui</a>, preencha os dados e importe-a.
+			</p>
+			
+			<br><br>
+	
 		</div>
 	</div>	
 </div>
