13d3c4018d169d5725058217860983d28355a068
Ralny Andrade <ralnyandrade@gmail.com>
2019-11-18 17:36:56 -0300

    feat: Importação de dados CSV formatos de sala

--- application/controllers/zata/Import.php
+++ application/controllers/zata/Import.php
@@ -165,6 +165,69 @@ class Import extends MY_Controller
 		}
 	}
 
+	
+	/**
+	 * Faz a importação do CSV de Formatos de Sala
+	 *
+	 * @access  public
+	 * @return  void
+	 */
+	function importar_csv_formatos_de_sala()
+	{
+
+		// Define as configurações para o upload do CSV
+		$config['upload_path'] = './files/import/';
+		$config['allowed_types'] = 'csv';
+		$config['max_size'] = '2000';
+
+		// Carregando library de upload
+		$this->load->library('upload', $config);
+
+		// Se o upload falhar, exibe mensagem de erro na view
+		if (!$this->upload->do_upload('csvfile'))
+		{
+			/**
+			 * Criando sessao com mensagem  
+			 */
+			$this->session->set_flashdata('msg', 'falha-importar');
+
+			redirect(base_url('eventos/config/formato-de-sala/listar'));
+		}
+		else
+		{
+			$file_data = $this->upload->data();
+
+			$file_path =  './files/import/' . $file_data['file_name'];
+
+			// Chama o método 'get_array', da library csvimport, passando o path do
+			// arquivo CSV. Esse método retornará um array.
+			$csv_array = $this->csvimport->get_array($file_path);
+
+			if ($csv_array)
+			{
+				// Faz a interação no array para poder gravar os dados na tabela 'contatos'
+				foreach ($csv_array as $row)
+				{
+					$data['desc_formato_de_sala']   = $row['desc_formato_de_sala'];
+
+					// Insere os dados na tabela 'contatos'
+					$this->import_model->insert_csv($this->input->post('tabela'), $data);
+				}
+
+				$this->session->set_flashdata('msg', 'importar-sucesso');
+
+				redirect(base_url('eventos/config/formato-de-sala/listar'));
+			}
+			else
+			{
+
+				$data['error'] = "Ocorreu um erro, desculpe!";
+
+				redirect(base_url('eventos/config/formato-de-sala/listar'));
+			}
+		}
+	}
+
 
 
 
