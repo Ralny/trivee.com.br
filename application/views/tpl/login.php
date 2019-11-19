a37dab5db0f457b77de5b6457e163e5bb7b9e631
Ralny Andrade <ralnyandrade@gmail.com>
2019-11-19 18:25:37 -0300

    refactor: Atualizar tpl user login dropdown

--- application/views/tpl/login.php
+++ application/views/tpl/login.php
@@ -1,30 +1,34 @@
+<!-- BEGIN USER LOGIN DROPDOWN -->
 <li class="dropdown dropdown-user dropdown-dark">
 	<a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
-	<img alt="" class="img-circle" src="<?= base_url() ?>assets/admin/layout3/img/avatar.png">
-	<span class="username username-hide-mobile"><? //$toggle_name ?></span>
+	<img alt="" class="img-circle" src="<?= base_url() ?>assets/admin/layout3/img/avatar9.jpg">
+	<span class="username username-hide-mobile">Ralny Andrade</span>
 	</a>
 	<ul class="dropdown-menu dropdown-menu-default">
-		<!--
 		<li>
 			<a href="extra_profile.html">
-			<i class="icon-user"></i> Meu Perfil</a>
+			<i class="icon-user"></i> Meu Perfil </a>
 		</li>
+		<!--
 		<li>
 			<a href="page_calendar.html">
-			<i class="icon-calendar"></i> Agenda</a>
+			<i class="icon-calendar"></i> My Calendar </a>
 		</li>
 		<li>
 			<a href="inbox.html">
-			<i class="icon-envelope-open"></i> Tarefas<span class="badge badge-danger">
+			<i class="icon-envelope-open"></i> My Inbox <span class="badge badge-danger">
 			3 </span>
 			</a>
 		</li>
-
-		<li class="divider"></li>
-	-->
+		-->
 		<li>
-			<a href="<?= base_url() ?>sair">
-			<i class="icon-key"></i> Log Out </a>
+			<a href="javascript:;"><i class="icon-rocket"></i> Minhas Tarefas <span class="badge badge-success">7</span></a>
+		</li>
+		<li class="divider">
+		</li>
+		<li>
+			<a href="<?= base_url() ?>sair"><i class="icon-key"></i> Sair </a>
 		</li>
 	</ul>
 </li>
+<!-- END USER LOGIN DROPDOWN -->
