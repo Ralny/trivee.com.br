a3b620cdf873ce7e6f01ba9327e8861997287b18
Ralny Andrade <ralnyandrade@gmail.com>
2019-11-21 11:25:27 -0300

    feat: Atualiza grid

--- application/views/eventos/reserva_evento/main-list.php
+++ application/views/eventos/reserva_evento/main-list.php
@@ -20,63 +20,151 @@ include ('application/views/tpl/config_container.php');
 				</div>
 			</div>
 			<div class="portlet-body">
+				<table class="table table-striped table-bordered table-hover" id="sample_2">
+					<thead>
+						<th width="10%">Status</th>
+						<th width="10%">Núm. reserva</th>
+						<th>Evento</th>
+						<th>Cliente</th>
+						<th width="10%">Valor total</th>
+						<th width="10%" style="text-align: center;">Detalhes</th>
+						<th width="10%">Ações</th>
+					</thead>
+					<tbody>
+					<?php 
+						/**
+						 * Listando registros da tabela
+						 * Listing table records
+						 */
+						foreach ($lista as $linha):
+							/**
+							 * Listar registros da tabela
+							 * Change Active Label
+							 */		            
+					?>	
+						<tr class="odd gradeX">									
+							<td>
+								<span class="label bg-blue"> <?= $linha->desc_status_reserva_evento ?> </span>
+							</td>
+							<td><?= $linha->numero_reserva ?></td>
+							<td><?= $linha->desc_evento ?></td>
+							<td><?= $linha->nome_fantasia ?></td>
+							<td><?= moeda($linha->valor_total_reserva_evento) ?></td>
+							<td style="text-align: center;">
+							<span class="btn btn-default popovers" data-container="body" data-trigger="hover" data-placement="left" data-content=
+							"
+							<table class='table table-hover table-light'>
+								<thead>
+									<tr class='uppercase'>
+										<th colspan='3'>
+											DETALHES DA RESERVA : <strong><?= $linha->numero_reserva ?></strong>
+										</th>
+									</tr>
+									</thead>
+								<tbody>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Inicio</a>
+										</td>
+										<td>
+											<span class='bold theme-font'><?= mostraData($linha->dth_previsao_inicio) ?></span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Pax</a>
+										</td>
+										<td>
+											<span class='bold theme-font'><?= $linha->num_pax ?></span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Salas</a>
+										</td>
+										<td>
+											<span class='bold theme-font'>R$ 3.500,00</span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>A&B</a>
+										</td>
+										<td>
+											<span class='bold theme-font'>R$ 2.000,00</span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Equipamentos</a>
+										</td>
+										<td>
+											<span class='bold theme-font'>R$ 500,00</span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Internet</a>
+										</td>
+										<td>
+											<span class='bold theme-font'>R$ 500,00</span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Pessoal</a>
+										</td>
+										<td>
+											<span class='bold theme-font'>R$ 300,00</span>
+										</td>
+									</tr>
+									<tr>
+										<td class='fit'>
+											
+										</td>
+										<td>
+											<a href='javascript:;' class='primary-link'>Outros</a>
+										</td>
+										<td>
+											<span class='bold theme-font'>R$ 300,00</span>
+										</td>
+									</tr>
+								</tbody>
+							</table>			
+							
+							"
+							aria-describedby="popover263156"><i class="fa fa-file-text-o"></i></span>
+							</td>
+							<td>
+								<a class=" btn btn-default" href="<?= base_url() . $url ?>/editar/<?= $linha->token_id ?>"><i class="fa fa-pencil"></i></a>
+								<a class=" btn btn-default" href="<?= base_url() . $url ?>/excluir/<?= $linha->token_id ?>"><i class="fa fa-trash-o"></i></a>
+							</td>
+						</tr>
+					<?php endforeach ?>	
+					</tbody>
+				</table>
 				
-					<table class="table table-striped table-bordered table-hover" id="sample_2">
-							<thead>
-								<th width="10%">Status</th>
-								<th width="10%">Núm. reserva</th>
-								<th>Evento</th>
-								<th>Cliente</th>
-								<th width="10%">Valor total</th>
-								<th width="10%" style="text-align: center;">Detalhes</th>
-								<th width="10%">Ações</th>
-							</thead>
-							<tbody>
-							<?php 
-								/**
-								 * Listando registros da tabela
-								 * Listing table records
-								 */
-								foreach ($lista as $linha):
-									/**
-									 * Listar registros da tabela
-									 * Change Active Label
-									 */		            
-					        ?>	
-								<tr class="odd gradeX">									
-									<td>
-										<span class="label bg-blue"> <?= $linha->desc_status_reserva_evento ?> </span>
-									</td>
-									<td><?= $linha->numero_reserva ?></td>
-									<td><?= $linha->desc_evento ?></td>
-									<td><?= $linha->nome_fantasia ?></td>
-									<td><?= moeda($linha->valor_total_reserva_evento) ?></td>
-									<td style="text-align: center;">
-									<span, class="btn btn-default popovers" data-container="body" data-trigger="hover" data-placement="left" 
-									data-content="
-												
-													Previsão de inicio: <strong>20/10/2019</strong>
-													<br/>
-													Numero de Pax: 209
-													<br/>
-													Qtde de Salas: 8
-													<br/>
-													Total de Salas: R$ 3.500,00
-													<br/>
-													Total de A&B: R$ 1.500,00
-													<br/>
-													Total de Outros: R$ 500,00
-									
-												" aria-describedby="popover263156"><i class="fa fa-file-text-o"></i></span>
-									</td>
-									<td>
-										<a class=" btn btn-default" href="<?= base_url() . $url ?>/editar/<?= $linha->token_id ?>"><i class="fa fa-pencil"></i></a>
-	                    				<a class=" btn btn-default" href="<?= base_url() . $url ?>/excluir/<?= $linha->token_id ?>"><i class="fa fa-trash-o"></i></a>
-									</td>
-								</tr>
-							<?php endforeach ?>	
-							</tbody>
-						</table>
 			</div>
 		</div>
 		<!-- END EXAMPLE TABLE PORTLET-->
